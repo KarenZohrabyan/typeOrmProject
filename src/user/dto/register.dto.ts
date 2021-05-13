38cc78ac05a7d31454bc06cdd27f37a996ef7b9f
karenzohrabyan <zohrabyankaren96@gmail.com>
2021-05-13 23:11:52 +0400

    I added swagger

--- src/user/dto/register.dto.ts
+++ src/user/dto/register.dto.ts
@@ -1,14 +1,18 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsString } from "class-validator";
 
 export class RegisterDto {
+    @ApiProperty()
     @IsNotEmpty()
     @IsString()
     name: string;
 
+    @ApiProperty()
     @IsNotEmpty()
     @IsString()
     email: string
 
+    @ApiProperty()
     @IsNotEmpty()
     @IsString()
     password: string
